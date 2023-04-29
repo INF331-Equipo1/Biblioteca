2eb8742fb9a38401dad78947301367b7d064a884
Diego Saa <diego.saa@envision.cl>
2023-04-29 11:51:38 -0400

    update

--- src/App.js
+++ src/App.js
@@ -9,6 +9,9 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
+        <p>
+          Test Publicación Web Site para Biblioteca Ramo Pruebas de Software
+        </p>
         <a
           className="App-link"
           href="https://reactjs.org"
