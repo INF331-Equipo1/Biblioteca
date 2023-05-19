a3e7cf6121df43be408e9fe7044efa3d6486171f
Diego Saa <diego.saa@envision.cl>
2023-05-18 21:08:42 -0400

    test CRUD edit libro

--- src/pages/Home/Home.test.jsx
+++ src/pages/Home/Home.test.jsx
@@ -19,5 +19,20 @@ describe("Home Component", () => {
     // Verificar que el diálogo de 'Crear libro' esté visible después de hacer clic en el botón
     expect(screen.getByText("Añadir libro")).toBeInTheDocument;
   });
+  it("Verificar que se abra el modal de 'Añadir libro' al hacer en el botón +", () => {
+    act(() => { render(<Home />); });
+    
+    // Verificar que el diálogo de 'Crear libro' no esté visible inicialmente
+    expect(screen.queryByText("Añadir libro")).toBeNull();
+
+    // Obtener el botón de 'Crear'
+    const crearButton = screen.getByLabelText("NuevoLibro");
+    
+    // Hacer clic en el botón de 'Crear'
+    fireEvent.click(crearButton);
+
+    // Verificar que el diálogo de 'Crear libro' esté visible después de hacer clic en el botón
+    expect(screen.getByText("Añadir libro")).toBeInTheDocument;
+  });
 });
 
